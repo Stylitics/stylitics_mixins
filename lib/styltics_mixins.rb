73b98b51d70fef8fa084e116f625e459745fc218
Joseph DelCioppio <joseph.delcioppio@gmail.com>
2013-08-01 12:36:07 -0400

    Have StyliticsMixins module to add SASS load paths

--- lib/styltics_mixins.rb
+++ lib/styltics_mixins.rb
@@ -1,4 +1,11 @@
-require 'stylitics_mixins/engine'
-
 module StyliticsMixins
+  def self.load!
+    require 'sass-rails'
+    require 'stylitics_mixins/engine'
+
+    stylesheets = File.expand_path(File.join("..", 'vendor', 'assets', 'stylesheets'))
+   ::Sass.load_paths << stylesheets
+  end
 end
+
+StyliticsMixins.load!
